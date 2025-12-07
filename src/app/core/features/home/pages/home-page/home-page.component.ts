2a50c1662d3492b90c042f33031d31cd6897002a
subl-1me <julio.ugalde404@gmail.com>
2025-12-07 10:55:50 -0700

    Home ~ Added start button

--- src/app/core/features/home/pages/home-page/home-page.component.ts
+++ src/app/core/features/home/pages/home-page/home-page.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { SuggestionBubbleComponent } from '../../components/suggestion-bubble/suggestion-bubble.component';
 import { AiBubbleComponent } from '../../components/ai-bubble/ai-bubble/ai-bubble.component';
-
 import { DEFAULT_ORB_SUGGESTIONS } from '../../../../shared/utils/constants';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [SuggestionBubbleComponent, AiBubbleComponent],
+  imports: [SuggestionBubbleComponent, AiBubbleComponent, NgIf],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
+  public isStarted: boolean;
   public defaultSuggestions: string[] = DEFAULT_ORB_SUGGESTIONS;
+
+  constructor() {
+    this.isStarted = false;
+  }
+
+  public toggleHasStarted(): void {
+    this.isStarted = !this.isStarted;
+  }
 }
