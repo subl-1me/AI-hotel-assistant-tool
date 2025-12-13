9b922c9cc7e7101de3ec0903c4a41869ff6ee144
subl-1me <julio.ugalde404@gmail.com>
2025-12-13 11:00:35 -0700

    Testing ~ Toolbar menu

---
+++ src/app/core/shared/testing-components/testing-toolbar/testing-toolbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { AIModelService } from '../../../services/ai-model.service';
+import { IntentService } from '../../../services/intent.service';
+
+@Component({
+  selector: 'app-testing-toolbar',
+  imports: [],
+  templateUrl: './testing-toolbar.component.html',
+  styleUrl: './testing-toolbar.component.css',
+})
+export class TestingToolbarComponent {
+  constructor(
+    private aiModelService: AIModelService,
+    private intentService: IntentService
+  ) {}
+
+  onTestButtonClick(text: string): void {
+    this.aiModelService.sendTextToModel(text).subscribe((response) => {
+      const { success, result } = response;
+      const { intent, entities, intent_confidence, text } = result;
+      this.intentService.emitNotification(text);
+    });
+  }
+}
