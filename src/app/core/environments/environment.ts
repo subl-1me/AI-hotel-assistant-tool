0866814913078f1ed68fabcd1cd87f5cd230be35
subl-1me <julio.ugalde404@gmail.com>
2026-01-17 09:06:02 -0700

    Updated API urls from environment

--- src/app/core/environments/environment.ts
+++ src/app/core/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  SQLITE_LOCAL_TESTING_DB: 'http://localhost:5000/api',
+  SQLITE_LOCAL_TESTING_DB: 'http://localhost:5009/api',
   VOICE_PROCESSING_API_URL: 'http://localhost:5001/api/process-audio',
   TEXT_MODEL_PROCESSING_API_URL: 'http://localhost:5001/api/process-text',
   production: false,
